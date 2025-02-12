992b5b5f5168d46c502535542ef011cd6bc18896
dlucianop <3112dlp@gmail.com>
2025-02-12 16:46:34 -0600

    Cambios no subidos

--- src/index.js
+++ src/index.js
@@ -8,6 +8,26 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'..' ,'static', 'inicio.html'));
 });
 
+/*Enrutamientos para Gobierno municipal */
+
+app.get('/gobierno-municipal', (req, res) => {
+    res.sendFile(path.join(__dirname,'..' ,'static', 'gob-municipal.html'));
+});
+
+app.get('/gobierno-municipal/presidencia', (req, res) => {
+    res.sendFile(path.join(__dirname,'..' ,'static', 'gob-municipal.html'));
+});
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000);
 
 console.log(`Server on port ${3000}`)
